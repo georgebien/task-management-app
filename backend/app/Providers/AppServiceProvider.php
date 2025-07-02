e08c702090952607297d262814e9f5787f75f8a6
George Bien A. Almenanza <gbienalmenanza@gmail.com>
2025-07-02 17:22:39 +0800

    [BACKEND]: Added auto creation of sqlite database file

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -7,18 +7,14 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $path = database_path('database.sqlite');
+
+        if (!file_exists($path)) {
+            file_put_contents($path, '');
+        }
     }
 }
