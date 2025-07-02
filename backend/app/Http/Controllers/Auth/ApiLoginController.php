6e1dd05ff34f981131de00636f44dfba0581b73f
George Bien A. Almenanza <gbienalmenanza@gmail.com>
2025-07-02 22:48:39 +0800

    [BACKEND]: Added ApiLoginController for logging in via postman

---
+++ backend/app/Http/Controllers/Auth/ApiLoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+class ApiLoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken('api-token')->plainTextToken,
+            'user' => $user,
+        ]);
+    }
+}
